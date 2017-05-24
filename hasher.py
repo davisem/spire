98617d587eb2be0b49feb1a8d3e80f720ec3cbd5
Eric Davis <emdavis48@gmail.com>
2017-05-24 14:21:41 -0600

    broke apart files, added optimization to avoid full kmer pair enumeration

---
+++ hasher.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+__author__ = "Eric Davis"
+__copyright__ = ""
+__credits__ = ["Eric Davis"]
+__version__ = ""
+__maintainer__ = "Eric Davis"
+__email__ = "emdavis48@gmail.com"
+__status__ = "wat wat"
+__modname__ = 'calc_jaccard'
+
+import numpy as np
+
+class Hasher(IHasher):
+	"""Some hashing function"""
+	
+	__slots__ = ['a', 'b']
+
+	def __init__(self, a, b):
+		self._a = a
+		self._b = b
+		self._c = max(a, b) - 100
+
+	def hashIt(self, x):
+		"""
+		Hashes the input.
+		:param int x: The input value to hash.
+		:return int: The hash value.
+		"""
+		return (x + self._b) % self._c
+
+
+class HashFactory(object):
+	"""
+	h(x) = (ax + b) % c
+	such that a and b are random ints < x. 
+	"""
+
+	MININT = np.iinfo(np.int32).min
+	MAXINT = np.iinfo(np.int32).max
+
+	def __init__(self, ihasher, n_hashes, seed):
+		"""
+		Init method for class
+		:param IHasher ihasher: A class implementing the ihasher interface
+		:param int n_hashes: How many random hashes to make
+		:param int seed: Seed for random number generation
+		"""
+		
+		self.ihasher = ihasher
+		self.n_hashes = n_hashes
+		self._seed =  seed
+
+	def getRandomInts(self):
+		"""
+		Makes a pair of random integers. 
+		Seed ensures the randomness is deterministic.
+		"""
+		
+		randoms = np.random.RandomState(seed=self._seed).randint(self.MININT, self.MAXINT, self.n_hashes * 2)
+		
+		for i in range(len(randoms) - 2):
+			a, b = randoms[i: i + 2]
+			yield a, b
+
+	def getHashes(self):
+		"""
+		Returns instances of random hashing classes.
+		:return list(IHasher): a list of IHasher instances.
+		"""
+		h_funcs = []
+		for a, b in self.getRandomInts():
+			 h_funcs.append(self.ihasher(a, b))
+		
+		return h_funcs
---
+++ kmerize.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+__author__ = "Eric Davis"
+__copyright__ = ""
+__credits__ = ["Eric Davis"]
+__version__ = ""
+__maintainer__ = "Eric Davis"
+__email__ = "emdavis48@gmail.com"
+__status__ = "wat wat"
+__modname__ = 'calc_jaccard'
+
+
+import sys
+import numpy as np
+from collections import defaultdict
+from abc import ABCMeta, abstractmethod
+
+
+class IKMERFactory(object):
+
+	def __init__(self):
+		self.words = self.makeWords()
+
+	def create(self, w, k):
+		return self.getHashValues()
+
+
+class KMERPairFactory(IKMERFactory):
+	"""Class for enumerating kmers from a window, c/o Patrick"""
+
+	@classmethod
+	def makeWords(cls, w, k):
+		pairs = {}
+		for i in range (len(w) - k):
+			for j in range (i + 1, len(w) - k):
+				a = w[i:i + k]
+				b = w[j:j + k]
+				d = j - i
+				ab = a + b
+				if ab in pairs:
+				    pairs[ab] = min(d, pairs[ab])
+				else :
+				    pairs[ab] = d
+
+		return pairs
+
+	def getHashValues(self):
+		"""Formats the kmer_pair, distance dict into a list of inter values"""
+
+		return [hash(keys+str(distances)) for keys, distances in self.words.iteritems()]
+
+
+class KMERSingleFactory(IKMERFactory):
+
+	@classmethod
+	def makeWords(cls, w, k):
+		
+		kmers = defaultdict(list)
+		
+		for i in xrange(len(w) - k + 1):
+			kmers[w[i: i + k]].append(i)
+
+		return kmers
+
+	def getHashValues(self, words):
+		return [hash(keys) for keys in words.iterkeys()]
