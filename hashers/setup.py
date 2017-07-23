e8377d002085278844ecd08fc452423a6b043ae4
Eric Davis <emdavis48@gmail.com>
2017-07-22 21:19:08 -0600

    moved setup.py

--- hashers/setup.py
+++ hashers/setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
 setup(name = "c_hasher",
-	ext_modules = cythonize("hashers/chash.pyx", include_dirs=['hashers']),
+	ext_modules = cythonize("chash.pyx"),
 )
